aa3855e559333c775880de17a213e67187dff973
Michał Mikołajczyk <38507424+Michal-Miko@users.noreply.github.com>
2022-03-27 21:18:35 +0200

    Added dev postgres container to run-dev.sh

--- run-dev.sh
+++ run-dev.sh
@@ -6,13 +6,17 @@ set -e
 FRONTEND_PREFIX="\033[38;5;154m FRONTEND_|\033[0m"
 BACKEND_PREFIX="\033[38;5;27m BACKEND__|\033[0m"
 
+# Optional -- db:
+docker run --rm -d --network host --name psql -e POSTGRES_PASSWORD=passwd postgres || \
+    echo "Error starting postgresql container, is it already running?"
+export DATABASE_URL=postgresql://postgres:@localhost/postgres
+
 # Frontend
 export REACT_APP_BACKEND_URL=http://localhost:8000/api
 npm start --prefix frontend 2>&1 | awk '{ print pfx, $0}' pfx="$FRONTEND_PREFIX" &
 
 # Backend
 source ./backend/env/bin/activate
-export DATABASE_URL=postgresql://postgres:@localhost/ctdb
 uvicorn --use-colors --app-dir backend --host 127.0.0.1 app.main:app --reload 2>&1 | awk '{ print pfx, $0}' pfx="$BACKEND_PREFIX"
 
 # Kill the background jobs on exit
