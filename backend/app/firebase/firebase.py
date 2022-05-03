cddfa05b4f6f40b3713544bfe1be8df4bdb4f0b9
Michał Mikołajczyk <38507424+Michal-Miko@users.noreply.github.com>
2022-05-03 22:32:00 +0200

    ExpiredTokenError handling

--- backend/app/firebase/firebase.py
+++ backend/app/firebase/firebase.py
@@ -10,8 +10,9 @@ def verify_token(token):
     except auth.RevokedIdTokenError:
         # Token revoked, inform the user to reauthenticate or signOut().
         HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Token is revoked, please reauthenticate.")
-        pass
     except auth.InvalidIdTokenError:
         # Token is invalid
         HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Token is invalid, please reauthenticate.")
-        pass
+    except auth.ExpiredTokenError:
+        # Token is expired
+        HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Token has expired, please reauthenticate.")
